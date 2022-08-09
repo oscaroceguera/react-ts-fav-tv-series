e0439e407cad21f85d90c4114a2110664cb50005
oscar <oscaroceguera@gmail.com>
2022-08-08 23:49:01 -0600

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { SeriesProps } from "./interfaces/SeriesProps";
+import "./App.css";
+import List from "./components/List";
+import Form from "./components/Form";
+
+function App() {
+  const [seriesList, setSeriesList] = useState<SeriesProps["seriesList"]>([]);
+
+  return (
+    <div className="App">
+      <h1>My favorite TV Series</h1>
+      <Form seriesList={seriesList} setSeriesList={setSeriesList} />
+      <List seriesList={seriesList} />
+    </div>
+  );
+}
+
+export default App;
